96f6995629b1a86a072dae29fe50745364f1d772
alco <you@example.com>
2025-03-01 20:36:43 +0400

    init

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 # Creating Private ECR Repository
 resource "aws_ecr_repository" "private_repo" {
   count                = length(var.repo_names)
@@ -7,7 +16,7 @@ resource "aws_ecr_repository" "private_repo" {
   tags = var.default_tags
 }
 
-# This resource can only be used in the us-east-1 region.
+# Creating Public ECR Repository. This resource can only be used in the us-east-1 region.
 resource "aws_ecrpublic_repository" "public_repo" {
   repository_name = "${var.default_tags["Project"]}-public-ecr-repo" # var.default_tags["Project"]` Taking value of key (Project) from map (var.default_tags map)
   
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "ecr_data" {
   value = {
-    "Repo Names" = aws_ecr_repository.private_repo[*].name
+    "Private Repo Names" = aws_ecr_repository.private_repo[*].name
     "Image Tag Mutability Type" = aws_ecr_repository.private_repo[*].image_tag_mutability
+    "Public Repo name" = aws_ecrpublic_repository.public_repo.repository_name
   }
 }
